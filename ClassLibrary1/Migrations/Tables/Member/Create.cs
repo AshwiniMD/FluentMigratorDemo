2c52ddfe79f803568d6ebe3ddac00d1add16ec1f
Ashwini Mutalik Desai <ashwinidesai2310@gmail.com>
2017-06-13 17:28:06 +0100

    Migration patch 1

---
+++ ClassLibrary1/Migrations/Tables/Member/Create.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+namespace FluentMigratorDemo.Migrations
+{
+    [Migration(1)]
+    public class CreateMemberTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Member")
+                .WithColumn("MemberId").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(50)
+                .WithColumn("Address").AsString()
+                .WithColumn("MobileNo").AsString(10);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Member");
+        }
+    }
+}
