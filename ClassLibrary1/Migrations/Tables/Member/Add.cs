61c33fea04a37b31153ff40549118f16da4477cd
Ashwini Mutalik Desai <ashwinidesai2310@gmail.com>
2017-06-13 17:29:29 +0100

    Migration patch 2

---
+++ ClassLibrary1/Migrations/Tables/Member/Add.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+namespace FluentMigratorDemo.Migrations
+{
+    [Migration(2)]
+    public class AddCountryColumn : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Member")
+                .AddColumn("Country").AsString();
+        }
+        public override void Down()
+        {
+            Delete.Column("Country").FromTable("Member");
+        }
+    }
+}
